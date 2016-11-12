0976b18d50de0c2157aee4d5dbae68f4dba65a38
SlinkyMation <jmmorris2@mail.bradley.edu>
2016-11-12 14:58:23 -0600

    added python test

---
+++ pythontest.py
@@ -0,0 +1,5 @@
+import time
+import webbrowser
+webbrowser.open('C:\Users\iJason\Documents\Smart_Calendar\Smart_HTML\Calendar.html')
+time.sleep(3)
+webbrowser.open('C:\Users\iJason\Documents\Smart_Calendar\Smart_HTML\Ads.html', new = 0)
