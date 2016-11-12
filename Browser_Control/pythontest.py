723a15ed2fdae771f8297205ee665e584e88d8b2
SlinkyMation <jmmorris2@mail.bradley.edu>
2016-11-12 15:02:04 -0600

    pythontest change

---
+++ Browser_Control/pythontest.py
@@ -0,0 +1,5 @@
+import time
+import webbrowser
+webbrowser.open('file://`pwd`/../Smart_HTML/Calendar.html')
+time.sleep(3)
+webbrowser.open('file://`pwd`/../Smart_HTML/Ads.html', new = 0)
--- pythontest.py
+++
@@ -1,5 +0,0 @@
-import time
-import webbrowser
-webbrowser.open('C:\Users\iJason\Documents\Smart_Calendar\Smart_HTML\Calendar.html')
-time.sleep(3)
-webbrowser.open('C:\Users\iJason\Documents\Smart_Calendar\Smart_HTML\Ads.html', new = 0)
