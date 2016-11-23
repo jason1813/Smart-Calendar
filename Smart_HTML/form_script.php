a2b5766fe7f9e7e60b7f26dfed9b68bb2d1288ca
SlinkyMation <jmmorris2@mail.bradley.edu>
2016-11-23 02:07:12 -0600

    gfdhg

--- Smart_HTML/form_script.php
+++ Smart_HTML/form_script.php
@@ -1,11 +1,41 @@
+<html>
+<head>
+    <title>Mali Messenger</title>
+    <link rel="stylesheet" type="text/css" href="./CSS/Smart_Style.css">
+</head>
+
+<body>
+
+    <!--tabs at the top-->
+    <ul class="toptabs">
+        <li><a href="index.html">Calendar</a></li>
+        <li><a href="Ads.html">Advertisements</a></li>
+        <li id="active"><a>Mali Messenger</a></li>
+    </ul>
+
+    <!--all page content. has left margins for the side tabs-->
+    <div class="content">
+
 <?PHP
 
 $email="jmmorris2@mail.bradley.edu";
+$phone="3097121597@vtext.com";
 $subject="Calendar Message";
 $message=$_POST['message'];
+$messageType=$_POST['messageType'];
+$deliveryMethod = $email;
 
-mail($email, $subject, $message);
+if ($messageType == "Urgent"){ 
+    $deliveryMethod = $phone; 
+    $subject="";
+    }
 
-print "Your message has been sent: </br>$email</br>$subject</br>$message</p>";
+mail($deliveryMethod, $subject, $message);
+
+print "Professor Mali has received your message!";
 
 ?>
+
+</div>
+</body>
+</html>
