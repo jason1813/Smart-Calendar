c6ab387db4d40236e1bf6d4b392b71f5220d88c5
SlinkyMation <jmmorris2@mail.bradley.edu>
2017-03-21 14:30:33 -0500

    changed pins.py only
    made it more like a state machine
    reads pins every 1 sec
    has timer to activate calendar
    has timer to timestamp someone is at door

--- Background/pins.py
+++ Background/pins.py
@@ -1,12 +1,8 @@
 import os, time, atexit, pwd
 
-#pinreaderUID = pwd.getpwnam('pinreader')[2]
-#os.setuid(pinreaderUID)
-
 def closepin():
 		os.system("echo 17 > /sys/class/gpio/unexport")
 		os.system("echo 27 > /sys/class/gpio/unexport")
-		os.system("echo 22 > /sys/class/gpio/unexport")
 
 atexit.register(closepin)
 
@@ -16,20 +12,78 @@ os.system("echo in > /sys/class/gpio/gpio17/direction")
 os.system("echo 27 > /sys/class/gpio/export")
 os.system("echo in > /sys/class/gpio/gpio27/direction")
 
-os.system("echo 22 > /sys/class/gpio/export")
-os.system("echo in > /sys/class/gpio/gpio22/direction")
-
+state = 0
+#timeout = 0
+caltimer = 0
+doortimer = 0
+doorm = 0
+calm = 0
+L_visited = 0
+R_visited = 0
+first = 0
 
 while(1):
-		time.sleep(2)
+		time.sleep(1)
 		file = open("/sys/class/gpio/gpio17/value", "r")
-		print 'left', file.read()
+		L = int(file.read())
 		file.close()
-		
 		file = open("/sys/class/gpio/gpio27/value", "r")
-		print 'middle', file.read()
+		R = int(file.read())
 		file.close()
 		
-		file = open("/sys/class/gpio/gpio22/value", "r")
-		print 'right', file.read()
-		file.close()
+		if R == 1 && doortimer < 5:
+			doortimer = doortimer+1
+		elif R == 1:
+			print 'doorstop'
+		else
+			doortimer = 0
+		
+		if L == 1 && caltimer < 5:
+			caltimer = caltimer+1
+		elif R == 1:
+			print 'calstop'
+		else
+			caltimer = 0
+		
+		if state == 0: #idle
+			print 'idle'
+			if L == 1 && R == 1:
+				state = 5 #error!
+			elif L == 1:
+				state = 1
+				first = 0
+			elif R == 1:
+				state = 2
+				first = 1
+		
+		elif state == 1: #LHold
+			print 'L Hold'
+			L_visited = 1
+			if L == 0 && R == 0:
+				state = 0
+				if first == 0:
+					print 'Came L left L'
+				else
+					print 'Came R left L'
+			elif L == 0 && R == 1:
+				state = 2
+			
+		elif state == 2: #RHold
+			print 'R Hold'
+			R_visited = 1
+			if L == 0 && R == 0:
+				state = 0
+				if first == 0:
+					print 'Came L left R'
+				else
+					print 'Came R left R'
+			elif L == 1 && R == 0:
+				state = 1
+			
+		elif state == 5: #wait
+			print 'Wait'
+			if L == 0 && R == 0:
+				state = 0
+		
+		
+		
