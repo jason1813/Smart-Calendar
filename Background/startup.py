86a1711bbc5ae85598d475d75fcf5d711ffd68ef
ColeL <clindeman@mail.bradley.edu>
2017-04-18 12:47:23 -0500

    added comments

--- Background/startup.py
+++ Background/startup.py
@@ -3,26 +3,28 @@ import os, sys, serial, time
 option = str(sys.argv[1:2])
 
 if(option == '[\'--start\']'):
-	#print 'start'
+	#create communication file
 	if(not os.path.isfile('/var/www/Smart-Calendar/HTML/temporary/message.txt')):
 		file = open('/var/www/Smart-Calendar/HTML/temporary/message.txt', 'w+')
 		file.close()
+	#create timestamp file
 	if(not os.path.isfile('/var/www/Smart-Calendar/HTML/temporary/stamp.txt')):
 		file = open('/var/www/Smart-Calendar/HTML/temporary/stamp.txt', 'w+')
 		file.write('File Created ' + time.asctime() + '\n')
 		file.close()
+	#turn on pins, set to input
 	os.system("echo 17 > /sys/class/gpio/export")
 	os.system("echo in > /sys/class/gpio/gpio17/direction")
 	os.system("echo 27 > /sys/class/gpio/export")
 	os.system("echo in > /sys/class/gpio/gpio27/direction")
 	os.system("python /var/www/Smart-Calendar/Background/pins.py &")
-	#download ads/announcements?
 
 elif(option == '[\'--stop\']'):
-	#print 'stop'
-	os.system("kill $(ps -eF | grep pins.py | grep -v grep | awk '{print $2}')")
+	os.system("kill $(ps -eF | grep pins.py | grep -v grep | awk '{print $2}')") #end pins.py
+	#turn off pins
 	os.system("echo 17 > /sys/class/gpio/unexport")
 	os.system("echo 27 > /sys/class/gpio/unexport")
+	#format timestamp file
 	timevar = time.asctime()
 	file = open('/var/www/Smart-Calendar/HTML/temporary/stamp.txt', 'a+')
 	file.write('File Closed ' + timevar + '\n')
@@ -32,13 +34,15 @@ elif(option == '[\'--stop\']'):
 	#email file
 
 elif(option == '[\'--monitor-on\']'):
-	os.system("tvservice -p; chvt 6; chvt 7")
+	os.system("tvservice -p; chvt 6; chvt 7") #turn on hdmi port
+	#close power to monitor through serial
 	ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 	code = 'A00101A2'
 	ser.write(code.decode('HEX'))
 
 elif(option == '[\'--monitor-off\']'):
-	os.system("tvservice -p; tvservice -o")
+	os.system("tvservice -p; tvservice -o") #turn off hdmi port
+	#open power to monitor through serial
 	ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 	code = 'A00100A1'
 	ser.write(code.decode('HEX'))
