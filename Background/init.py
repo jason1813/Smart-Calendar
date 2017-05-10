6dec5b253c16d0c6aad4a51061ef17019a5b34fe
olekmali <olekmali@ieee.org>
2017-05-10 14:04:27 -0500

    Discontinue cycling the monitor off and on on startup

--- Background/init.py
+++ Background/init.py
@@ -1,8 +1,2 @@
 import os, sys, time
-
-os.system("python /var/www/Smart-Calendar/Background/startup.py --monitor-off") #dont let anyone see desktop
 os.system("python /var/www/Smart-Calendar/Background/startup.py --start") #startup services
-
-time.sleep(60) #wait in seconds
-
-os.system("python /var/www/Smart-Calendar/Background/startup.py --monitor-on") #browser should have loaded by now
