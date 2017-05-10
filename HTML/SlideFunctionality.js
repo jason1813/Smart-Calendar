6e2721b0413e61031d22ff06625cbf59e10558c7
olekmali <olekmali@ieee.org>
2017-05-10 15:30:57 -0500

    troubleshooting slide show

--- HTML/SlideFunctionality.js
+++ HTML/SlideFunctionality.js
@@ -5,7 +5,7 @@ window.setInterval(carousel, 20000); // Change image every 20 seconds
 function carousel()
 {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = window.document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++)
     {
         x[i].style.display = "none";
@@ -16,6 +16,7 @@ function carousel()
         slideIndex = 0;
     }
     x[slideIndex].style.display = "block";
+    window.alert(slideIndex);
 }
 
 carousel();
